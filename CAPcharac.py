337c40e219ae8cdb6c99bb936fffa076ce3a954b
BurrisNick <212257434+BurrisNick@users.noreply.github.com>
2026-04-22 14:57:38 -0400

    added plots

--- CAPcharac.py
+++ CAPcharac.py
@@ -48,4 +48,51 @@ def CAPcharac(dataPath):
     latency = timeENGPeak[0]
     halfwidth = hwidth[0]
 
+
+    ###################################################
+    ### plotting the pulse and ENG along with the raw data
+    plt.plot(time, eng)
+    plt.plot(time, eng1)
+    plt.plot(time, pulse)
+    plt.plot(time, pulse1)
+    plt.plot(timeENGPeak, magPeak, 'o')
+
+    if magPeak.shape[0] > 1:
+        titleText = f'AP mag 1: {magPeak[0] * 1000:.1f}mV, mag 2: {magPeak[1] * 1000:.1f}mV' \
+                    f'\nLatency 1st: {timeENGPeak[0] * 1000:.1f}ms, 2nd {timeENGPeak[1] * 1000:.1f}ms' \
+                    f'\nhalfwidth 1st: {hwidth[0]:.1f}ms, 2nd: {hwidth[1]:.1f}ms'
+    else:
+        titleText = f'AP mag: {magPeak[0] * 1000:.1f}mV' \
+                    f'\nLatency: {timeENGPeak[0] * 1000:.1f}ms' \
+                    f'\nhalfwidth: {hwidth[0]:.1f}ms'
+
+    plt.title(titleText)
+    plt.xlabel('time (ms)')
+    plt.ylabel('magnitude (V)')
+    plt.show()
+
+    plt.show()
+
+    # order = 3
+    # band = [50, 3000]
+    # print(sr)
+    # sos = signal.butter(order,band, btype='bandpass', fs=sr, output='sos')
+    # filtered_ekg = signal.sosfiltfilt(sos, eng)
+
+
+    # need to add function for second peak if detected.
+    # use for loop for this
+    if magPeak.shape[0] > 1:
+        avLatency = np.mean(latency[0])
+        avAmps = np.mean(amplitude[0])
+        avWidth = np.mean(halfwidth[0])
+    else:
+        avLatency = np.mean(latency)
+        avAmps = np.mean(amplitude)
+        avWidth = np.mean(halfwidth)
+
+    print(f'average amplitude is: {avAmps * 1000: .2f}mV\n'
+          f'average latency is: {avLatency * 1000: .2f}ms\n'
+          f'average half width is: {avWidth:.2f}ms\n')
+
     return amplitude, latency, halfwidth
